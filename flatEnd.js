a45c224950911c3818b2866042757d336a5a0b4b
Harsh <harshjaiswal28555@gmail.com>
2023-03-23 22:10:37 +0530

    flatEnd

---
+++ flatEnd.js
@@ -0,0 +1,12 @@
+function FlatEndArr(arr) {
+  let flat = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i]) === false) {
+       flat.push(arr[i]);
+    } else {
+       flat=flat.concat(FlatEndArr(arr[i]))     
+    }
+  }
+  return flat
+}
+console.log(FlatEndArr([1, 2, [3, 4],[[4,5],7]]));
