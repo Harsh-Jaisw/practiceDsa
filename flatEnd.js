fa59925d767e928a084e2226a0271860274d9b12
Harsh <harshjaiswal28555@gmail.com>
2023-03-24 00:07:26 +0530

    flatEnd

--- flatEnd.js
+++ flatEnd.js
@@ -1,12 +1,26 @@
-function FlatEndArr(arr) {
-  let flat = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (Array.isArray(arr[i]) === false) {
-       flat.push(arr[i]);
-    } else {
-       flat=flat.concat(FlatEndArr(arr[i]))     
-    }
+// function FlatEndArr(arr) {
+//   let flat = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (Array.isArray(arr[i]) === false) {
+//        flat.push(arr[i]);
+//     } else {
+//        flat=flat.concat(FlatEndArr(arr[i]))     
+//     }
+//   }
+//   return flat
+// }
+// console.log(FlatEndArr([1, 2, [3, 4],[[4,5],7]]));
+
+function FlatEndArr(arr){
+  let flat=[]
+ for(let i=0;i<arr.length;i++){
+  if(Array.isArray(arr[i])===false){
+    flat.push(arr[i])
   }
-  return flat
+  else{
+    flat=flat.concat(FlatEndArr(arr[i]))
+  }
+ }
+ return flat
 }
-console.log(FlatEndArr([1, 2, [3, 4],[[4,5],7]]));
+console.log(FlatEndArr([1,2,3,[4,5],[3,7,[6,8]],9]))
