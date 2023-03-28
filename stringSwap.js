d363469ee9089d778dfdf6da07f6d5d48305cf1d
Harsh <harshjaiswal28555@gmail.com>
2023-03-28 18:01:09 +0530

    swap String

---
+++ stringSwap.js
@@ -0,0 +1,13 @@
+    // let x="harsh" //harshjaiswal
+    // let y="jaiswal"
+    // let n=x.length//5
+    // x=x.concat(y)
+    // y=x.slice(0,n)
+    // x=x.slice(n,x.length)
+    // console.log(x," ",y)
+    let x='shalini'
+    let y='tiwari'
+    x=x.concat(y)
+    y=x.slice(0,x.length-y.length)
+    x=x.slice(y.length)
+    console.log(x,y)
