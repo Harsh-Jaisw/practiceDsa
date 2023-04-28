e1b6c11e9cbe5166ac3b26ddb8017bc0dae4afe2
Harsh <harshjaiswal28555@gmail.com>
2023-04-28 16:48:40 +0530

    update

--- practice1.js
+++ practice1.js
@@ -2,17 +2,13 @@
 // //kadane's Algorithum
 // let max_sum=-Infinity
 // let sum=0
-  
-
-
-
 // for(let i=0;i<arr.length;i++){
 //     sum=sum+arr[i]
 //     max_sum=Math.max(max_sum,sum)
 //     if(sum<0){
 //         sum=0
 //     }
-    
+
 // }
 // console.log(max_sum)
 
@@ -34,3 +30,38 @@
 //     return true;
 // }
 // console.log(isAnagram('hrahs','harsh'))
+
+
+
+// for hold solution is not appropriate
+// function Anagram(str1, str2) {
+//   let mp = new Map();
+//   let mp1 = new Map();
+//   for (let i = 0; i < str1.length; i++) {
+//     if (mp.has(str1[i])) {
+//       mp.set(str1[i], mp.get(str1[i]) + 1);
+//     } else {
+//       mp.set(str1[i], 1);
+//     }
+//   }
+
+//   for (let i = 0; i < str2.length; i++) {
+//     if (mp1.has(str2[i])) {
+//       mp1.set(str2[i], mp1.get(str2[i]) + 1);
+//     } else {
+//       mp1.set(str2[i], 1);
+//     }
+//   }
+//   mp.forEach((value, key) => {
+//     if (mp1.get(key)!==value) {
+//       return false;
+//     }
+//   });
+//   mp1.forEach((value, key) => {
+//     if (mp.get(key)!==value) {
+//       return false;
+//     }
+//   });
+//   return true;
+// }
+// console.log(Anagram("madam", "maadc"));
